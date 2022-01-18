3c21d6c15d55c5b75e8652370513b811591ce444
luucaslfs <lfs@cin.ufpe.br>
2022-01-18 15:17:26 -0300

    building model

---
+++ model.py
@@ -0,0 +1,31 @@
+from random import Random
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import pickle
+
+# Loading .csv file
+df = pd.read_csv("iris.csv")
+
+print(df.head())
+
+# Select independent and dependent variable
+X = df["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]
+Y = df["Class"]
+
+# Split the dataset into train and test
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=50)
+
+# Feature scaling 
+sc = StandardScaler()
+X_train =  sc.fit_transform(X_train)
+X_test = sc.tansform(X_test)
+
+# Instantiate the model
+classifier = RandomForestClassifier()
+
+# Fit the model
+classifier.fit(X_train, y_train)
+
+# Make pickle file 
+pickle.dump(classifier, open("model.pkl", "wb"))
