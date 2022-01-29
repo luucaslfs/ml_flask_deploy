0a237aa6851e238de5f38c38c1b2348272b85b66
luucaslfs <lfs@cin.ufpe.br>
2022-01-29 19:26:44 -0300

    app code

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
-from random import Random
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pickle
 
@@ -9,17 +9,17 @@ df = pd.read_csv("iris.csv")
 
 print(df.head())
 
-# Select independent and dependent variable
-X = df["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]
+# Select independent and dependent variables
+X = df[["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]]
 Y = df["Class"]
 
-# Split the dataset into train and test
+# Split dataset into train and test
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=50)
 
 # Feature scaling 
 sc = StandardScaler()
 X_train =  sc.fit_transform(X_train)
-X_test = sc.tansform(X_test)
+X_test = sc.transform(X_test)
 
 # Instantiate the model
 classifier = RandomForestClassifier()
